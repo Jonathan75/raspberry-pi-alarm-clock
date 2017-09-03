bf2234b3a03c32151043704ad2f7d6b26f91b692
Jonathan Stevens <jonathanstevens75@gmail.com>
2017-09-03 19:28:50 -0400

    30 second timeout

--- buzzer.py
+++ buzzer.py
@@ -1,12 +1,27 @@
-import time
+import time, datetime
 from gpiozero import LED, PWMLED, Button
 
 buzzer = PWMLED(17)
 button = Button(21)
 
+start = datetime.datetime.now()
 
-while (button.is_pressed != True ):
+def endtime():
+	return (datetime.datetime.now()-datetime.timedelta(seconds=30)) 
+
+def timeout():
+	return endtime() > start
+
+def still_alarm():
+	if button.is_pressed:
+		return False
+	else:
+		return (timeout() == False)	
+
+while still_alarm()
 	buzzer.value = 1
 	time.sleep(.5)
 	buzzer.value = 0
 	time.sleep(2)
+
+
